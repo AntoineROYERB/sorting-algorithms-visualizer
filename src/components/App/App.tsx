2ea92212e5514263419c462a3667b3c431aa073c
Antoine ROYER BOURNELLE <royerb.antoine@gmail.com>
2024-02-06 10:26:09 +0100

    feat: Refactor code and fix styling issues

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,12 +1,12 @@
-import React, { useState } from "react";
+import React from "react";
 import "./App.css";
 import ResponsiveAppBar from "../Menu/Menu.tsx";
 import Rectangles from "../Rectangles/Rectangles.tsx";
 
 function App() {
-  const [sliderValue, setSliderValue] = useState(30);
+  const [sliderValue, setSliderValue] = React.useState(30);
 
-  const handleSliderChange = (newValue) => {
+  const handleSliderChange = (newValue: number) => {
     setSliderValue(newValue);
   };
   return (
--- src/components/Rectangles/Rectangles.tsx
+++ src/components/Rectangles/Rectangles.tsx
@@ -1,13 +1,57 @@
 import React from "react";
 import "./Rectangles.css";
 
-const RepeatDiv = ({ numberOfTimes }) => (
-  <>
-    {Array.from({ length: numberOfTimes }).map((_, index) => (
-      <div key={index} className="rectangle"></div>
-    ))}
-  </>
-);
+const BubbleSort = (arr: Array<Number>) => {
+  for (var i = 0; i < arr.length; i++) {
+    // Last i elements are already in place
+    for (var j = 0; j < arr.length - i - 1; j++) {
+      // Checking if the item at present iteration
+      // is greater than the next iteration
+      if (arr[j] > arr[j + 1]) {
+        // If the condition is true
+        // then swap them
+        var temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
+    }
+  }
+
+  // Print the sorted array
+  return arr;
+};
+
+const generateRandomArray = ({ n, size = 100 }) => {
+  let randomArray: Array<number> = [];
+  let randomNumber: number;
+
+  // // Initialisation du générateur pseudo-aléatoire
+  // Math.seedrandom(seed);
+
+  // Génération du tableau d'entiers aléatoires
+  for (let i = 0; i < n; i++) {
+    randomNumber = Math.floor(Math.random() * (size + 1));
+    randomArray.push(randomNumber as never);
+  }
+  return randomArray;
+};
+
+const RepeatDiv = ({ numberOfTimes }) => {
+  const ArrayToSort = generateRandomArray({ n: numberOfTimes });
+
+  console.log(ArrayToSort);
+  return (
+    <>
+      {ArrayToSort.map((rectangleHeight, index) => (
+        <div
+          key={index}
+          className="rectangle"
+          style={{ height: `${rectangleHeight}%` }}
+        ></div>
+      ))}
+    </>
+  );
+};
 
 export default function Rectangles({ sliderValue }) {
   return (
